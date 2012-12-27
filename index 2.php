d50857e09f28f482db47ed19cae3772a0f7a67eb
binhdna <binhdna@gmail.com>
2012-12-27 18:23:18 +0700

    file
    good

---
+++ index 2.php
@@ -0,0 +1,13 @@
+<?php
+function sayHi($name){
+	return "hi there";
+}
+echo sayHi('name');
+echo "anbinh";
+
+echo "<p> hello world </p>";
+echo "<p> hello world </p>";
+echo "<p> hello world </p>";
+
+echo "";
+
